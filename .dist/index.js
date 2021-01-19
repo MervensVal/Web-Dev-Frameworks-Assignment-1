b020469730dd0bc89bfe0c780c76978dac256f5f
Mervens Val <valmervens@gmail.com>
2021-01-19 17:30:53 -0500

    Completed all requirements minus data validations

--- .dist/index.js
+++ .dist/index.js
@@ -5,21 +5,31 @@ var comment = document.getElementById('comment');
 var rating = document.getElementById('rating');
 var template = document.getElementById('template');
 var resposepoint = document.getElementById('resposepoint');
+var currentdate = document.getElementById('currentdate');
 var average = document.getElementById('average');
 var sum = 0;
 var totalReviews = 0;
+var numberOfReviews = document.getElementById('numberOfReviews');
 btnSubmit.addEventListener('click', (event) => {
     var reviewerNameValue = reviewerName.value;
     var commentValue = comment.value;
     var ratingValue = rating.value;
-    var template = getTemplateClone('template');
-    template.content.getElementById('outputname').innerText = reviewerNameValue.toString();
-    template.content.getElementById('outputrating').innerText = ratingValue.toString();
-    template.content.getElementById('outputcomment').innerText = commentValue.toString();
-    sum = sum + +ratingValue;
-    totalReviews++;
-    average.innerText = getAverage(sum, totalReviews).toString();
-    resposepoint?.appendChild(template.content);
+    var currentdate = new Date();
+    var error = document.getElementById('ratingerror');
+    if (+ratingValue > 5 || +ratingValue < 1) {
+        error.innerText = "*Rating will be applied only if value is 1 - 5";
+    }
+    else {
+        var template = getTemplateClone('template');
+        template.content.getElementById('outputname').innerText = reviewerNameValue.toString();
+        template.content.getElementById('outputrating').innerText = ratingValue.toString();
+        template.content.getElementById('outputcomment').innerText = commentValue.toString();
+        template.content.getElementById('currentdate').innerText = currentdate.toString();
+        sum = sum + +ratingValue;
+        totalReviews++;
+        average.innerText = getAverage(sum, totalReviews).toFixed(1);
+        resposepoint?.appendChild(template.content);
+    }
 });
 function getTemplateClone(TemplateID) {
     return document.getElementById('template')?.cloneNode(true);
--- index.ts
+++ index.ts
@@ -4,27 +4,39 @@ var comment = document.getElementById('comment') as HTMLInputElement;
 var rating = document.getElementById('rating') as HTMLInputElement;
 var template = document.getElementById('template') as HTMLTemplateElement;
 var resposepoint = document.getElementById('resposepoint');
+var currentdate = document.getElementById('currentdate');
 
 var average = document.getElementById('average') as HTMLInputElement;
 var sum = 0;
 var totalReviews = 0;
+
+var numberOfReviews = document.getElementById('numberOfReviews');
 
 btnSubmit.addEventListener('click',(event)=> {
 
     var reviewerNameValue = reviewerName.value;
     var commentValue = comment.value;
     var ratingValue = rating.value;
+    var currentdate = new Date();
+    
+    var error = document.getElementById('ratingerror');
 
-    var template = getTemplateClone('template');
-    template.content.getElementById('outputname')!.innerText=reviewerNameValue.toString();
-    template.content.getElementById('outputrating')!.innerText=ratingValue.toString();
-    template.content.getElementById('outputcomment')!.innerText=commentValue.toString();
+    if(+ratingValue > 5 || +ratingValue < 1){
+            error!.innerText = "*Rating will be applied only if value is 1 - 5";
+         }
+    else {
 
-    sum = sum + +ratingValue;
-    totalReviews++;
-    average.innerText = getAverage(sum,totalReviews).toString();
+            var template = getTemplateClone('template');
+            template.content.getElementById('outputname')!.innerText=reviewerNameValue.toString();
+            template.content.getElementById('outputrating')!.innerText=ratingValue.toString();
+            template.content.getElementById('outputcomment')!.innerText=commentValue.toString();
+            template.content.getElementById('currentdate')!.innerText=currentdate.toString();
 
-    resposepoint?.appendChild(template.content);
+            sum = sum + +ratingValue;
+            totalReviews++;
+            average.innerText = getAverage(sum,totalReviews).toFixed(1);
+            resposepoint?.appendChild(template.content);
+         }
 });
 
 function getTemplateClone(TemplateID:string) :HTMLTemplateElement{
@@ -36,3 +48,5 @@ function getAverage(sum:number, numTimes:number):number{
     var average = sum/numTimes;
     return average;
 }
+
+
