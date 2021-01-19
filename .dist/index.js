3342ed2c41e12cdc7f591924262d1548f5b902a3
Mervens Val <valmervens@gmail.com>
2021-01-19 14:06:37 -0500

    Program complete minus CSS and Validations

--- .dist/index.js
+++ .dist/index.js
@@ -5,6 +5,9 @@ var comment = document.getElementById('comment');
 var rating = document.getElementById('rating');
 var template = document.getElementById('template');
 var resposepoint = document.getElementById('resposepoint');
+var average = document.getElementById('average');
+var sum = 0;
+var totalReviews = 0;
 btnSubmit.addEventListener('click', (event) => {
     var reviewerNameValue = reviewerName.value;
     var commentValue = comment.value;
@@ -13,9 +16,16 @@ btnSubmit.addEventListener('click', (event) => {
     template.content.getElementById('outputname').innerText = reviewerNameValue.toString();
     template.content.getElementById('outputrating').innerText = ratingValue.toString();
     template.content.getElementById('outputcomment').innerText = commentValue.toString();
+    sum = sum + +ratingValue;
+    totalReviews++;
+    average.innerText = getAverage(sum, totalReviews).toString();
     resposepoint?.appendChild(template.content);
 });
 function getTemplateClone(TemplateID) {
     return document.getElementById('template')?.cloneNode(true);
+}
+function getAverage(sum, numTimes) {
+    var average = sum / numTimes;
+    return average;
 }
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 var btnSubmit = document.getElementById('submitbtn') as HTMLButtonElement;
-
 var reviewerName = document.getElementById('reviewername')as HTMLInputElement;
-
-
 var comment = document.getElementById('comment') as HTMLInputElement;
-
-
 var rating = document.getElementById('rating') as HTMLInputElement;
-
-
 var template = document.getElementById('template') as HTMLTemplateElement;
 var resposepoint = document.getElementById('resposepoint');
+
+var average = document.getElementById('average') as HTMLInputElement;
+var sum = 0;
+var totalReviews = 0;
 
 btnSubmit.addEventListener('click',(event)=> {
 
@@ -23,9 +20,19 @@ btnSubmit.addEventListener('click',(event)=> {
     template.content.getElementById('outputrating')!.innerText=ratingValue.toString();
     template.content.getElementById('outputcomment')!.innerText=commentValue.toString();
 
+    sum = sum + +ratingValue;
+    totalReviews++;
+    average.innerText = getAverage(sum,totalReviews).toString();
+
     resposepoint?.appendChild(template.content);
 });
 
 function getTemplateClone(TemplateID:string) :HTMLTemplateElement{
     return document.getElementById('template')?.cloneNode(true) as HTMLTemplateElement;
+}
+
+
+function getAverage(sum:number, numTimes:number):number{
+    var average = sum/numTimes;
+    return average;
 }
